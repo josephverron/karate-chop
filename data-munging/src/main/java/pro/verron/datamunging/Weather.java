bc0fe67ebd015af9c887dd54006728800edebdd0
Joseph Verron <joseph.verron@totalenergies.com>
2022-11-09 15:42:43 +0100

    data-munching

--- data-munging/src/main/java/pro/verron/datamunging/Weather.java
+++ data-munging/src/main/java/pro/verron/datamunging/Weather.java
@@ -1,14 +1,48 @@
 package pro.verron.datamunging;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static java.util.Comparator.comparing;
 
 public class Weather {
     public static void main(String[] args) throws IOException {
-        String filepath = "pro/verron/datamunging/weather.dat";
-        InputStream stream = ClassLoader.getSystemResourceAsStream(filepath);
-        assert stream != null : "Couldn't find the data set file at position: %s".formatted(filepath);
-        System.out.println(new String(new BufferedInputStream(stream).readAllBytes()));
+        try (Stream<String> lines = Files.lines(Paths.get("weather.dat"))) {
+            String day = lines
+                    .skip(2)
+                    .limit(30)
+                    .map(toWeatherRow())
+                    .min(comparing(WeatherRow::tempRange))
+                    .map(WeatherRow::day)
+                    .map(String::valueOf)
+                    .orElse("None");
+            System.out.println("Day with lowest temperature range: " + day);
+        }
+    }
+
+    private static Function<String, WeatherRow> toWeatherRow() {
+        return str -> new WeatherRow(
+                extract(str, 0, 4, Integer::parseInt),
+                extract(str, 4, 10, Double::parseDouble),
+                extract(str, 10, 16, Double::parseDouble)
+        );
+    }
+
+    private static <T> T extract(String str, int beginIndex, int endIndex, Function<String, T> function) {
+        var dayString = str.substring(beginIndex, endIndex).trim().replace("*", "");
+        return function.apply(dayString);
+    }
+
+    private record WeatherRow(
+            int day,
+            double maxTemp,
+            double minTemp
+    ) {
+        public double tempRange() {
+            return maxTemp - minTemp;
+        }
     }
 }
