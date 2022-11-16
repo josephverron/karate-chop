1859e8873b9a077764c77ad345144544c75e3558
Joseph Verron <joseph.verron@totalenergies.com>
2022-11-16 15:03:16 +0100

    conflicting-objectives

---
+++ conflicting-objectives/run.py
@@ -0,0 +1,17 @@
+from itertools import product
+
+shortlist = {1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
+
+with open('wordlist.txt', 'r') as dic:
+    for line in dic.readlines():
+        word = line.strip()
+        if len(word) < 7:
+            shortlist[len(word)].append(word.strip())
+
+results = []
+
+for leftSize, rightSize in zip(range(1, 6), reversed(range(1, 6))):
+    for left, right in product(shortlist[leftSize], shortlist[rightSize]):
+        if left + right in shortlist[6]:
+            results.append((left, right))
+    print((leftSize, rightSize), len(results))
