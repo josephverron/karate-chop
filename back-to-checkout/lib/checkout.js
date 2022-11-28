99579a7ed0b80431099820d7bbb539f72e337251
Joseph Verron <joseph.verron@totalenergies.com>
2022-11-28 09:22:45 +0100

    test 2

--- back-to-checkout/lib/checkout.js
+++ back-to-checkout/lib/checkout.js
@@ -1,10 +1,24 @@
 const unit_prices = {
     "A": 50,
-    "B": 30
+    "B": 30,
+    "C": 20,
+    "D": 15
 }
 
-export function price(str){
-    if(str in unit_prices)
-        return unit_prices[str]
-    return 0
+export class checkout {
+    constructor() {
+        this.total = 0
+    }
+
+    scan(str) {
+        this.total = this.total + price(str);
+    }
+}
+
+export function price(str) {
+    let total = 0;
+    for (const item of str.split(""))
+        if (item in unit_prices)
+            total += unit_prices[item]
+    return total
 }
--- back-to-checkout/spec/checkout.spec.js
+++ back-to-checkout/spec/checkout.spec.js
@@ -1,13 +1,23 @@
 import {price} from "../lib/checkout.js";
 
 describe('Checkout Pricing', function() {
-    it('Empty basket cost nothing', function() {
+    it('Empty basket cost nothing', function () {
         expect(price("")).toEqual(0);
     });
-    it('1 A costs 50', function() {
+    it('1 A costs 50', function () {
         expect(price("A")).toEqual(50);
     });
-    it('1 B costs 30', function() {
+    it('1 B costs 30', function () {
         expect(price("B")).toEqual(30);
+    });
+    it('1 C costs 20', function () {
+        expect(price("C")).toEqual(20);
+    });
+    it('1 D costs 15', function () {
+        expect(price("D")).toEqual(15);
+    });
+    it('AB costs 80', function () {
+        expect(price("AB")).toEqual(80);
+        expect("ABCD".split("")).toEqual(["A", "B", "C", "D"])
     });
 });
