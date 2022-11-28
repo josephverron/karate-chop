d9b96818bce6cc13f5eef8ff02f50a53c35e928f
Joseph Verron <joseph.verron@totalenergies.com>
2022-11-28 12:46:04 +0100

    adapt to exercise

--- back-to-checkout/lib/checkout.js
+++ back-to-checkout/lib/checkout.js
@@ -1,24 +1,18 @@
-const unit_prices = {
-    "A": 50,
-    "B": 30,
-    "C": 20,
-    "D": 15
-}
-
 export class checkout {
-    constructor() {
-        this.total = 0
+    #total = 0;
+
+    constructor(prices) {
+        this.prices = prices
     }
 
-    scan(str) {
-        this.total = this.total + price(str);
+    scan(item) {
+        if (item in this.prices)
+            this.#total += this.prices[item];
+        else
+            throw new Error(`Unknown price for item ${item} in ${Object.entries(this.prices).reduce((accumulator, entry) => accumulator + entry[0] + ":" + entry[1] + " ", "")}`)
     }
-}
 
-export function price(str) {
-    let total = 0;
-    for (const item of str.split(""))
-        if (item in unit_prices)
-            total += unit_prices[item]
-    return total
+    total() {
+        return this.#total
+    }
 }
--- back-to-checkout/spec/checkout.spec.js
+++ back-to-checkout/spec/checkout.spec.js
@@ -1,23 +1,58 @@
-import {price} from "../lib/checkout.js";
+import {checkout} from "../lib/checkout.js";
 
-describe('Checkout Pricing', function() {
-    it('Empty basket cost nothing', function () {
+const prices = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15
+}
+
+function price(str) {
+    const co = new checkout(prices)
+    str.split("").forEach((item) => co.scan(item))
+    return co.total()
+}
+
+describe('Checkout Pricing', function () {
+    it('incremental', function () {
+        const co = new checkout(prices)
+        expect(co.total()).toEqual(0)
+
+        co.scan("A");
+        expect(co.total()).toEqual(50)
+
+        co.scan("B");
+        expect(co.total()).toEqual(80)
+
+        co.scan("A");
+        expect(co.total()).toEqual(130)
+
+        //co.scan("A");
+        //expect(co.total()).toEqual(160)
+
+        //co.scan("B");
+        //expect(co.total()).toEqual(175)
+    });
+
+    it('total from singles', function () {
         expect(price("")).toEqual(0);
-    });
-    it('1 A costs 50', function () {
         expect(price("A")).toEqual(50);
-    });
-    it('1 B costs 30', function () {
-        expect(price("B")).toEqual(30);
-    });
-    it('1 C costs 20', function () {
-        expect(price("C")).toEqual(20);
-    });
-    it('1 D costs 15', function () {
-        expect(price("D")).toEqual(15);
-    });
-    it('Several single items addition', function () {
         expect(price("AB")).toEqual(80);
         expect(price("CDBA")).toEqual(115);
+    });
+
+    it('total from multiples', function () {
+        expect(price("AA")).toEqual(100);
+        //expect(price("AAA")).toEqual(130);
+        //expect(price("AAAA")).toEqual(180);
+        //expect(price("AAAAA")).toEqual(230);
+        //expect(price("AAAAAA")).toEqual(260);
+    });
+
+    xit('total from composition', function () {
+        //expect(price("AAAB")).toEqual(160);
+        //expect(price("AAABB")).toEqual(175);
+        //expect(price("AAABBD")).toEqual(190);
+        //expect(price("DABABA")).toEqual(190);
     });
 });
