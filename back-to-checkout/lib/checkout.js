c6330aea4862006f888f706105bd89bb727b35c0
Joseph Verron <joseph.verron@totalenergies.com>
2022-11-29 09:17:14 +0100

    rafactor before next step

--- back-to-checkout/lib/checkout.js
+++ back-to-checkout/lib/checkout.js
@@ -1,19 +1,23 @@
 export class checkout {
-    #total = 0;
     #prices = {}
+    items = []
 
     constructor(prices) {
         this.#prices = prices
     }
 
     scan(item) {
-        if (item in this.#prices)
-            this.#total += this.#prices[item][1];
-        else
-            throw new Error(`Unknown price for item ${item} in ${JSON.stringify(this.#prices)}`)
+        this.items.push(item);
     }
 
     total() {
-        return this.#total
+        let total = 0;
+        for (const item of this.items) {
+            if (item in this.#prices)
+                total += this.#prices[item][1];
+            else
+                throw new Error(`Unknown price for item ${item} in ${JSON.stringify(this.#prices)}`)
+        }
+        return total
     }
 }
